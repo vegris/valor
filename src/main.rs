f2cd1a0a6bbd496b3003ba592261a4b089ffa82f
cheaterok <1cheaterok1@gmail.com>
2020-05-02 13:27:22 +0300

    Добавляет показ def формата №1.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::SeekFrom;
 use std::convert::TryInto;
+use std::ops::Deref;
 
 extern crate flate2;
 use flate2::read::ZlibDecoder;
@@ -77,33 +78,36 @@ impl LodIndex {
 
 #[derive(Debug)]
 struct DefIndex {
+    data: Vec<u8>,
     type_: u32,
     palette: Vec<Color>,
-    registry: HashMap<u32, HashMap<String, u32>>
+    registry: HashMap<String, u32>,
+    blocks: HashMap<u32, Vec<String>>
 }
 
 impl DefIndex {
     fn open(lod_index: &mut LodIndex, filename: &String) -> Self {
-        let file_contents = lod_index.read_file(filename);
-        let (header, payload) = &file_contents.split_at(16);
+        let data = lod_index.read_file(filename);
+        let (header, payload) = &data.split_at(16);
 
         let type_ = u32::from_le_bytes(header[0..4].try_into().unwrap());
         let n_blocks = u32::from_le_bytes(header[12..16].try_into().unwrap());
 
-        let (palette_data, data) = payload.split_at(256*3);
+        let (palette_slice, mut cur_data) = payload.split_at(256*3);
 
-        let palette = palette_data.chunks(3)
+        let palette = palette_slice.chunks(3)
                              .map(|slice| Color::RGB(slice[0], slice[1], slice[2]))
                              .collect::<Vec<Color>>();
 
-        let mut registry: HashMap<u32, HashMap<String, u32>> = HashMap::with_capacity(n_blocks as usize);
-        let mut cur_data = data; // Wow thats ugly! Should probably use some folds instead
+        let mut registry: HashMap<String, u32> = HashMap::new();
+        let mut blocks: HashMap<u32, Vec<String>> = HashMap::with_capacity(n_blocks as usize);
+        // Wow thats ugly! Should probably use some folds instead
         for _ in 0..n_blocks {
-            let (block_header, data) = cur_data.split_at(16);
+            let (block_header, other) = cur_data.split_at(16);
             let block_id = u32::from_le_bytes(block_header[..4].try_into().unwrap());
             let n_entries = u32::from_le_bytes(block_header[4..8].try_into().unwrap()) as usize;
 
-            let (block_data, data) = data.split_at((13 + 4) * n_entries);
+            let (block_data, other) = other.split_at((13 + 4) * n_entries);
             let (names_buf, offsets_buf) = block_data.split_at(13 * n_entries);
             let names = names_buf
                         .chunks(13)
@@ -113,18 +117,69 @@ impl DefIndex {
             let offsets = offsets_buf
                           .chunks(4)
                           .map(|bytes| u32::from_le_bytes(bytes.try_into().unwrap()));
-            
-            let block_registry = Iterator::zip(names, offsets).collect::<HashMap<String, u32>>();
-            registry.insert(block_id, block_registry);
-            cur_data = data;
+
+            let mut block: Vec<String> = Vec::with_capacity(n_entries as usize);
+            for (name, offset) in Iterator::zip(names, offsets) {
+                block.push(name.clone());
+                registry.insert(name, offset);
+            }
+            blocks.insert(block_id, block);
+            cur_data = other;
         }
         dbg!(&type_);
         dbg!(&registry);
-        DefIndex {type_, palette, registry}
+        dbg!(&blocks);
+        DefIndex {data, type_, palette, registry, blocks}
+    }
+
+    fn load_image_data(&self, name: &String) -> (u32, u32, Vec<u8>) {
+        let offset = *self.registry.get(name).unwrap();
+        let data = &self.data.as_slice()[offset as usize..];
+        let (header, image_data) = data.split_at(32);
+        let [_size, format, fw, fh, w, h, lm, tm]: [u32; 8] = header
+            .chunks(4)
+            .map(TryInto::try_into)
+            .map(Result::unwrap)
+            .map(u32::from_le_bytes)
+            .collect::<Box<[u32]>>()
+            .deref().try_into().unwrap();
+
+        dbg!(format, fw, fh, w, h, lm, tm);
+
+        let mut pixel_data: Vec<u8> = Vec::with_capacity((w * h) as usize);
+        match format {
+            0 => pixel_data.extend_from_slice(&image_data[..(w * h) as usize]),
+            1 => {
+                let line_offsets = image_data[..(4 * h) as usize]
+                                    .chunks(4)
+                                    .map(|chunk| chunk.try_into().unwrap())
+                                    .map(u32::from_le_bytes);
+                for line_offset in line_offsets {
+                    let mut row = &image_data[line_offset as usize..];
+                    let mut total_row_length = 0;
+                    while total_row_length < w {
+                        let (code, length) = (u8::from_le(row[0]), u8::from_le(row[1]) as u32 + 1); // idk why we need to +1 length
+                        match code {
+                            0xff => {
+                                pixel_data.extend_from_slice(&row[2..length as usize + 2]);
+                                row = &row[length as usize + 2..];
+                            }
+                            _ => {
+                                    for _ in 0..length { pixel_data.push(code) }; 
+                                    row = &row[2..];
+                            }
+                        }
+                        total_row_length += length;
+                    }
+                }
+            },
+            _ => panic!("Unknown format!")
+        }
+        (w, h, pixel_data)
     }
 }
 
-fn surface_from_data(data: &mut Vec<u8>) -> Surface{
+fn surface_from_data(data: &mut Vec<u8>) -> Surface {
     // ЭТО РАЗМЕР КАРТИНКИ, А НЕ БУФЕРА data
     let size = u32::from_le_bytes(data[..4].try_into().unwrap());
     let width = u32::from_le_bytes(data[4..8].try_into().unwrap());
@@ -152,40 +207,44 @@ fn surface_from_data(data: &mut Vec<u8>) -> Surface{
 fn main() {
     let mut manager = LodIndex::open(LOD_ARCHIVE);
 
-    DefIndex::open(&mut manager, &String::from("CMAGOG.def"));
+    let def_index = DefIndex::open(&mut manager, &String::from("TBDnHal4.def"));
+    let (width, height, mut pixel_data) = def_index.load_image_data(&String::from("TBDHall4.pcx"));
 
     // let mut contents = manager.read_file(&String::from("CBONE2A2.PCX"));
 
-    // let sdl_context = sdl2::init().unwrap(); 
-    // let video_subsystem = sdl_context.video().unwrap();
+    let sdl_context = sdl2::init().unwrap(); 
+    let video_subsystem = sdl_context.video().unwrap();
 
-    // let window = video_subsystem.window("Rust", 800, 600)
-    //     .position_centered()
-    //     .build()
-    //     .unwrap();
+    let window = video_subsystem.window("Rust", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
     
-    // let mut canvas = window.into_canvas()
-    //     .present_vsync()
-    //     .build()
-    //     .unwrap();
+    let mut canvas = window.into_canvas()
+        .present_vsync()
+        .build()
+        .unwrap();
 
+    let mut surface = Surface::from_data(pixel_data.as_mut_slice(), width, height, 1 * width, PixelFormatEnum::Index8).unwrap();
     // let surface = surface_from_data(&mut contents);
-    // let texture_creator = canvas.texture_creator();
-    // let texture = texture_creator.create_texture_from_surface(&surface).unwrap();
+    let palette = Palette::with_colors(def_index.palette.as_slice()).unwrap();
+    surface.set_palette(&palette).unwrap();
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.create_texture_from_surface(&surface).unwrap();
 
-    // let mut event_pump = sdl_context.event_pump().unwrap();
-    // 'running: loop {
-    //     for event in event_pump.poll_iter() {
-    //         match event {
-    //             Event::Quit {..} |
-    //             Event::KeyDown { keycode: Some(Keycode::Escape), ..} => { 
-    //                 break 'running 
-    //             },
-    //             _ => {}
-    //         }
-    //     }
-    //     canvas.clear();
-    //     canvas.copy(&texture, None, surface.rect()).unwrap();
-    //     canvas.present();
-    // }
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), ..} => { 
+                    break 'running 
+                },
+                _ => {}
+            }
+        }
+        canvas.clear();
+        canvas.copy(&texture, None, surface.rect()).unwrap();
+        canvas.present();
+    }
 }
