97938527a77d6ef292efb11245653086ec6c5adb
cheaterok <1cheaterok1@gmail.com>
2020-05-31 19:01:13 +0300

    Рабочая версия с анимацией.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod resources;
 use resources::ResourceRegistry;
 use resources::catalog::battlefields::{Battlefields, BATTLEFIELDS_GRAPHICS};
 
-const LOD_ARCHIVE: &'static str = "/home/vsevolod/Wine/HoMM3/drive_c/HoMM3/Data/H3bitmap.lod";
+const LOD_ARCHIVE: &'static str = "/home/vsevolod/Wine/HoMM3/drive_c/HoMM3/Data/H3sprite.lod";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let sdl_context = sdl2::init()?; 
@@ -21,7 +21,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build()?;
 
     let mut resource_registry = ResourceRegistry::new(&canvas, &[&LOD_ARCHIVE]);
-    let texture = resource_registry.get_texture(LOD_ARCHIVE, BATTLEFIELDS_GRAPHICS[Battlefields::MAG as usize]);
+    // let texture = resource_registry.get_texture(LOD_ARCHIVE, BATTLEFIELDS_GRAPHICS[Battlefields::MAG as usize]);
+    let animation = resource_registry.get_animation(LOD_ARCHIVE, "CCHAMP.def");
+    dbg!(&animation.frames.keys());
+    let texture = animation.frames.values().next().unwrap();
     let texture_query = texture.query();
     let texture_rect = sdl2::rect::Rect::new(0, 0, texture_query.width, texture_query.height);
 
--- src/resources/resource_registry.rs
+++ src/resources/resource_registry.rs
@@ -9,10 +9,12 @@ use sdl2::surface::Surface;
 use sdl2::pixels::{Color, PixelFormatEnum, Palette};
 
 use super::lod_index::LodIndex;
+use super::def_parser;
+use super::def_parser::DefIndex;
 
-struct AnimationData<'a> {
-    frames: HashMap<String, Texture<'a>>,
-    blocks: HashMap<u8, Box<[String]>>
+pub struct Animation<'a> {
+    pub frames: HashMap<String, Texture<'a>>,
+    blocks: HashMap<u32, Box<[String]>>
 }
 
 pub struct ResourceRegistry {
@@ -39,6 +41,13 @@ impl ResourceRegistry {
         let lod_handle = self.resource_handles.get_mut(archive).unwrap();
         let pcx_data = lod_handle.read_file(file);
         pcx_data_to_texture(&self.texture_creator, pcx_data.deref())
+    }
+
+    pub fn get_animation(&mut self, archive: &str, file: &str) -> Animation {
+        let lod_handle = self.resource_handles.get_mut(archive).unwrap();
+        let def_data = lod_handle.read_file(file);
+        let def_index = def_parser::parse(&def_data);
+        def_index_to_animation(&self.texture_creator, def_index)
     }
 }
 
@@ -74,4 +83,15 @@ fn pcx_data_to_texture<'a>(tc: &'a TextureCreator<WindowContext>, pcx_data: &[u8
         };
     
     tc.create_texture_from_surface(surface).unwrap()
+}
+
+fn def_index_to_animation<'a>(tc: &'a TextureCreator<WindowContext>, def_index: DefIndex) -> Animation<'a> {
+    let mut frames = HashMap::new();
+    for (name, mut pix_data) in def_index.sprites_index {
+        let mut surface = Surface::from_data(&mut pix_data.pixels, pix_data.width, pix_data.height, 1 * pix_data.width, PixelFormatEnum::Index8).unwrap();
+        surface.set_palette(&def_index.palette).unwrap();
+        let frame = tc.create_texture_from_surface(surface).unwrap();
+        frames.insert(name, frame);
+    }
+    Animation{ frames, blocks: def_index.blocks }
 }
