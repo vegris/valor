eea2e9a637af6b8b2a336bf0403f5fbe10e68ecf
cheaterok <1cheaterok1@gmail.com>
2024-03-06 11:28:57 +0300

    Use units() to get stack handles

--- game/src/gamestate/turns.rs
+++ game/src/gamestate/turns.rs
@@ -2,7 +2,7 @@ use super::{GameState, Side, StackHandle};
 use crate::stack::Stack;
 
 pub fn find_active_stack(state: &GameState) -> Option<StackHandle> {
-    let mut handles: Box<[StackHandle]> = state.stacks.keys().copied().collect();
+    let mut handles = state.units();
     // Преимущество при равенстве скоростей у того кто ходил вторым на прошлом ходу
     handles
         .sort_unstable_by_key(|&handle| state.get_stack(handle).side == state.turn.priority_side);
